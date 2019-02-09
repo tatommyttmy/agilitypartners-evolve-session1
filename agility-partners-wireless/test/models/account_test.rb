886a00d574c6b7cc42b199292d38098c895ff98b
tatommyttmy <tatommyttmy@gmail.com>
2019-02-09 07:59:23 -0500

    Account and Model model validation tests

--- agility-partners-wireless/test/models/account_test.rb
+++ agility-partners-wireless/test/models/account_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without name" do
+    account = Account.new
+    assert_not account.save
+  end
+
+  test "should find name by fragment" do
+    account = Account.new(name: 'John Doe')
+    account.save
+    search_results = Account.name_search('oh')
+    assert_includes(search_results, account)
+  end
 end
--- agility-partners-wireless/test/models/model_test.rb
+++ agility-partners-wireless/test/models/model_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ModelTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid without name" do
+    model = Model.new
+    assert model.invalid?
+  end
+
+  test "cannot have negative quantity_on_hand" do
+    model = Model.new(name: 'Phone', quantity_on_hand: -1)
+    assert model.invalid?
+  end
+
+  test "name must be unique" do
+    model = Model.new(name: 'Phone', quantity_on_hand: 2)
+    model.save
+    other_model = Model.new(name: 'Phone', quantity_on_hand: 2)
+    assert_not other_model.save
+  end
 end
