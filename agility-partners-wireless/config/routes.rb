17357fbbf624328d95477cb2c80f2c6c893f136c
tatommyttmy <tatommyttmy@gmail.com>
2019-02-09 00:24:47 -0500

    agility-partners-wireless new rails project

--- agility-partners-wireless/config/routes.rb
+++ agility-partners-wireless/config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  resources :employees
-  resources :companies
-
-  root 'companies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
