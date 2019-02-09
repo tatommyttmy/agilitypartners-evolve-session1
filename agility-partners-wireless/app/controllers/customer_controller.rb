306268a5b4c27bfa97c3756e57ed434880eb795e
tatommyttmy <tatommyttmy@gmail.com>
2019-02-09 00:42:22 -0500

    add provider, customer, phone controllers

---
+++ agility-partners-wireless/app/controllers/customer_controller.rb
@@ -0,0 +1,10 @@
+class CustomerController < ApplicationController
+    def index
+        # put something in session says customer
+        # enter phone number
+    end
+
+    def login
+        # redirect to devices index with account id
+    end
+end
---
+++ agility-partners-wireless/app/controllers/provider_controller.rb
@@ -0,0 +1,7 @@
+class ProviderController < ApplicationController
+    def index
+        # links to inventory and accounts
+        # put something in the session that says you're provider
+        # links on page to devices index, account index
+    end
+end
